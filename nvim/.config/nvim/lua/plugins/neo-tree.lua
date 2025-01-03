32b7e9d1021b81c09e619439b286d58151b7a12c
Adrian Fernandez <adrifer@microsoft.com>
2025-01-03 10:27:23 -0800

    Add tmux navigation

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,6 @@ return {
   },
   config = function()
     vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
-    vim.keymap.set("n", "<leader>N", ":Neotree close<CR>")
   end
 }
 
