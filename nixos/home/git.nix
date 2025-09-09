64f6e54836a1e105459ca928431cf8c7319b7b65
Adrian Fernandez Garcia <tracker086@outlook.com>
2025-09-09 22:58:17 +0000

    Add everything needed for cloning AzDevOps projects

--- nixos/home/git.nix
+++ nixos/home/git.nix
@@ -3,7 +3,25 @@
 {
   programs.git = {
     enable = true;
-    userName = "Adrian Fernandez Garcia";
+
+    # Default identity
+    userName  = "Adrian Fernandez Garcia";
     userEmail = "tracker086@outlook.com";
+
+    # Extra config snippets
+    extraConfig = {
+      credential."https://dev.azure.com".useHttpPath = true;
+    };
+
+    # Conditional includes (per-directory)
+    includes = [
+      {
+        condition = "gitdir:~/work/";
+        contents.user = {
+          name  = "Adrian Fernandez Garcia";
+          email = "adrifer@microsoft.com";
+        };
+      }
+    ];
   };
 }
--- nixos/modules/wsl-only.nix
+++ nixos/modules/wsl-only.nix
@@ -1,17 +1,18 @@
-{ lib, pkgs, ... }:
-{
+{ lib, pkgs, ... }: {
   wsl.enable = true;
   wsl.defaultUser = lib.mkDefault "adrifer";
 
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-  ];
+  environment.systemPackages = with pkgs; [ wl-clipboard ];
 
   # Workaround for WSL NixOS vscode remote
   programs.nix-ld.enable = true;
+
+  home-manager.users.adrifer.programs.git.extraConfig = {
+    credential.helper =
+      "/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager.exe";
+  };
 
   # You can put WSL niceties here later (interop, automount, etc.)
   # Example:
   # wsl.wslConf.automount.root = "/mnt";
 }
-
