c0064a5232d988640ea131506e7b42d3ae263b08
Adrian Fernandez Garcia <tracker086@outlook.com>
2025-09-10 19:19:01 +0000

    Update common system.nix

--- nixos/modules/common-system.nix
+++ nixos/modules/common-system.nix
@@ -3,4 +3,9 @@
   systemd.tmpfiles.rules = [
     "L+ /usr/bin/bash - - - - ${pkgs.bash}/bin/bash"
   ];
+  
+  # Set unprivileged port start to 443 (for work project)
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_unprivileged_port_start" = 443;
+  };
 }
