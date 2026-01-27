c71762c37437489386da24e2b3790426903095e9
Adrian Fernandez Garcia <tracker086@outlook.com>
2026-01-27 22:32:38 +0000

    Add new stuff for syncthing

--- nixos/hosts/syncthing-lxc/configuration.nix
+++ nixos/hosts/syncthing-lxc/configuration.nix
@@ -20,6 +20,46 @@
 
     # Open firewall for Syncthing
     openDefaultPorts = true;
+
+    settings = {
+      folders = {
+        "TrackVault" = {
+          path = "/var/lib/syncthing/TrackVault";
+          devices = [ "ADRIFER-Ultron" "iPhone" ];
+        };
+      };
+      devices = {
+        "ADRIFER-Ultron" = { id = "SUYK5EV-GZD6WAJ-DOHS524-7RMYH54-23I4TYS-YN6HYL2-V4FCOEN-MBBG7AG"; };
+        "iPhone" = { id = "Z2LB3T3-MW5JCBR-ZAFY6XE-SUIKOV4-JAEGMDZ-JQIZLZC-2W346Y6-XI2WXQX"; };
+      };
+    };
+  };
+
+  # Auto backup TrackVault to GitHub every 6 hours
+  systemd.services.vault-git-backup = {
+    description = "Backup TrackVault to GitHub";
+    path = [ pkgs.git pkgs.openssh ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "syncthing";
+      WorkingDirectory = "/var/lib/syncthing/TrackVault";
+    };
+    script = ''
+      if [[ -n $(git status --porcelain) ]]; then
+        git add .
+        git commit -m "Auto backup $(date '+%Y-%m-%d %H:%M')"
+        git push
+      fi
+    '';
+  };
+
+  systemd.timers.vault-git-backup = {
+    description = "Run Vault backup every 6 hours";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 00/6:00:00";  # Every 6 hours
+      Persistent = true;  # Run if missed while powered off
+    };
   };
 
   # Open GUI port in firewall
