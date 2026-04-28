62117eb5d89241be7ea43f5214f413a2134f0745
Adrian Fernandez Garcia <tracker086@outlook.com>
2026-04-28 19:09:20 +0000

    Update and try to fix estlin_d issue in nvim

--- nvim/.config/nvim/lua/plugins/mason-tool-installer.lua
+++ nvim/.config/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,19 +1,31 @@
 return {
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	event = "VeryLazy",
+	lazy = false,
 	dependencies = {
 		"williamboman/mason.nvim",
 	},
-	opts = {
-		ensure_installed = {
-			"stylua",
-			"prettier",
-			"eslint_d",
-			"nixfmt",
-			"csharpier",
-		},
-		auto_update = false,
-		run_on_start = true,
-		start_delay = 3000,
-	},
+	config = function()
+		local mason_tool_installer = require("mason-tool-installer")
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"eslint_d",
+				"nixfmt",
+				"csharpier",
+			},
+			auto_update = false,
+			run_on_start = true,
+			start_delay = 0,
+		})
+
+		vim.api.nvim_create_autocmd("VimEnter", {
+			group = vim.api.nvim_create_augroup("mason_tools_force_install", { clear = true }),
+			once = true,
+			callback = function()
+				mason_tool_installer.check_install(false)
+			end,
+		})
+	end,
 }
