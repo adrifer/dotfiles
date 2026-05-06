c82e7e9ebf5e1bf68a66476a120facd0d11ea5e4
Adrian Fernandez Garcia <tracker086@outlook.com>
2026-05-06 04:06:02 +0000

    Add needed packages to wsl-work

--- nixos/hosts/wsl-work.nix
+++ nixos/hosts/wsl-work.nix
@@ -9,17 +9,25 @@ in
     modules = [
       config.flake.nixosModules.profile-base
       config.flake.nixosModules.profile-wsl
-      {
-        networking.hostName = "wsl-work";
-        system.stateVersion = "25.05";
+      (
+        { pkgs, ... }:
+        {
+          networking.hostName = "wsl-work";
+          system.stateVersion = "25.05";
 
-        boot.kernel.sysctl = {
-          "net.ipv4.ip_unprivileged_port_start" = 443;
-          "fs.inotify.max_user_watches" = 1048576;
-          "fs.inotify.max_user_instances" = 1024;
-          "fs.inotify.max_queued_events" = 65536;
-        };
-      }
+          boot.kernel.sysctl = {
+            "net.ipv4.ip_unprivileged_port_start" = 443;
+            "fs.inotify.max_user_watches" = 1048576;
+            "fs.inotify.max_user_instances" = 1024;
+            "fs.inotify.max_queued_events" = 65536;
+          };
+
+          home-manager.users.adrifer.home.packages = with pkgs; [
+            socat # Need this for copilot being able to query app insights
+            bubblewrap # Need this for copilot being able to query app insights
+          ];
+        }
+      )
     ];
   };
 }
