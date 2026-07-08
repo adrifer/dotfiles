5b5eba579f228c06eaabb845833be83002dfeb6b
Adrian Fernandez <tracker086@outlook.com>
2026-07-08 17:20:47 +0000

    Add herdr neovim plugin

--- nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -15,10 +15,50 @@ return {
 			disable_when_zoomed = true,
 		})
 
-		vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { desc = "Window: move left" })
-		vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "Window: move down" })
-		vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "Window: move up" })
-		vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "Window: move right" })
+		local function is_herdr()
+			return vim.env.HERDR_PANE_ID ~= nil and vim.env.HERDR_PANE_ID ~= ""
+		end
+
+		local function herdr_bin()
+			if vim.env.HERDR_BIN_PATH ~= nil and vim.env.HERDR_BIN_PATH ~= "" then
+				return vim.env.HERDR_BIN_PATH
+			end
+
+			return "herdr"
+		end
+
+		local function herdr_navigate(wincmd, direction)
+			local previous = vim.api.nvim_get_current_win()
+			vim.cmd.wincmd(wincmd)
+
+			if vim.api.nvim_get_current_win() ~= previous then
+				return
+			end
+
+			vim.fn.system({ herdr_bin(), "pane", "focus", "--direction", direction, "--current" })
+		end
+
+		local function navigate(wincmd, direction, tmux_navigate)
+			if is_herdr() then
+				herdr_navigate(wincmd, direction)
+				return
+			end
+
+			tmux_navigate()
+		end
+
+		vim.keymap.set("n", "<C-h>", function()
+			navigate("h", "left", nvim_tmux_nav.NvimTmuxNavigateLeft)
+		end, { desc = "Window: move left" })
+		vim.keymap.set("n", "<C-j>", function()
+			navigate("j", "down", nvim_tmux_nav.NvimTmuxNavigateDown)
+		end, { desc = "Window: move down" })
+		vim.keymap.set("n", "<C-k>", function()
+			navigate("k", "up", nvim_tmux_nav.NvimTmuxNavigateUp)
+		end, { desc = "Window: move up" })
+		vim.keymap.set("n", "<C-l>", function()
+			navigate("l", "right", nvim_tmux_nav.NvimTmuxNavigateRight)
+		end, { desc = "Window: move right" })
 		vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = "Window: last active" })
 		vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext, { desc = "Window: next" })
 	end,
--- winix/features/herdr.ts
+++ winix/features/herdr.ts
@@ -4,4 +4,14 @@ export const herdr = feature("herdr", ({ home }) => {
   home.packages(
     nix.expr("inputs.herdr.packages.${pkgs.stdenv.hostPlatform.system}.default"),
   );
+  home.activation("installHerdrVimNavigation", {
+    script: `
+      HERDR="\${inputs.herdr.packages.\${pkgs.stdenv.hostPlatform.system}.default}/bin/herdr"
+      JQ="\${pkgs.jq}/bin/jq"
+
+      if ! "$HERDR" plugin list --json | "$JQ" -e '.result.plugins[]? | select(.plugin_id == "vim-herdr-navigation")' >/dev/null; then
+        "$HERDR" plugin install paulbkim-dev/vim-herdr-navigation --yes
+      fi
+    `,
+  });
 });
