59b48ac7bb3771d90d609690bad4bbde0ca95de0
Adrian Fernandez <tracker086@outlook.com>
2026-07-08 17:23:45 +0000

    Update herdr activation script

--- winix/features/herdr.ts
+++ winix/features/herdr.ts
@@ -8,6 +8,8 @@ export const herdr = feature("herdr", ({ home }) => {
     script: `
       HERDR="\${inputs.herdr.packages.\${pkgs.stdenv.hostPlatform.system}.default}/bin/herdr"
       JQ="\${pkgs.jq}/bin/jq"
+      export HOME="\${config.home.homeDirectory}"
+      export PATH="\${pkgs.git}/bin:\${pkgs.openssh}/bin:\${pkgs.bash}/bin:\${pkgs.coreutils}/bin:$PATH"
 
       if ! "$HERDR" plugin list --json | "$JQ" -e '.result.plugins[]? | select(.plugin_id == "vim-herdr-navigation")' >/dev/null; then
         "$HERDR" plugin install paulbkim-dev/vim-herdr-navigation --yes
