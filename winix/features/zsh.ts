3c89257768ad8f2042ce9a8494e7bd27ceacf434
Adrian Fernandez <tracker086@outlook.com>
2026-09-13 03:22:33 +0000

    Fix u and update flake.lock

--- winix/features/zsh.ts
+++ winix/features/zsh.ts
@@ -110,17 +110,16 @@ export const zsh = feature("zsh", ({ home, nixos, platforms }) => {
       "..": "cd ..",
       "...": "cd ../..",
       n: "nvim",
+      u: "cd ~/dotfiles/winix && npm install && npx @adrifer/winix update",
       winix: "npx @adrifer/winix",
       ...(platforms.darwin.isActive && {
         i: "cd ~/dotfiles/winix && npm install && npx @adrifer/winix switch --host macbook-pro",
-        u: "cd ~/dotfiles/winix && npm install && npx @adrifer/winix update && npx @adrifer/winix switch --host macbook-pro",
         gc: "nix-collect-garbage -d",
         "foundry-dev":
           "sudo CHOKIDAR_USEPOLLING=true CHOKIDAR_INTERVAL=1000 pnpm dev",
       }),
       ...(platforms.nixos.isActive && {
         i: "cd ~/dotfiles/winix && npm install && npx @adrifer/winix switch",
-        u: "cd ~/dotfiles/winix && npm install && npx @adrifer/winix update && npx @adrifer/winix switch",
         gc: "sudo nix-collect-garbage -d",
       }),
     },
