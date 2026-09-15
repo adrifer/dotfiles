4350fe4a37984ead694bd38ac92d3b243ade1eca
Adrian Fernandez <tracker086@outlook.com>
2026-09-15 20:18:43 +0000

    Update trackagents

--- winix/features/trackagents.ts
+++ winix/features/trackagents.ts
@@ -2,5 +2,8 @@ import { feature } from "@adrifer/winix";
 
 export const trackagents = feature("trackagents", ({ home, platforms }) => {
   home.imports("inputs.trackagents.homeManagerModules.default");
-  home.program("trackagents", {});
+  home.program("trackagents", {
+    runtime: "self-updating",
+    updateOrigin: "https://ai.adrifer.com",
+  });
 });
